c83596742f14b239e1ed4e2f784a9558ce468e5a
Ashley Maxime <admin@ashleywavy.com>
2019-04-10 21:50:29 -0400

    First commit of code

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import MovieCard from './components/MovieCard.js'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        
+      </div>
+    );
+  }
+}
+
+export default App;
